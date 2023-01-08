bd403c3d967f5b571248cfbda3a637ad0f4cf66a
Miika Savela <miika.a.savela@gmail.com>
2023-01-08 14:41:56 +0200

    Add aligning the header items to sides of the view on desktop

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,7 +2,7 @@
 	import Navigation from "./Navigation/Navigation.svelte";
 </script>
 <header class="ph-m pb-s">
-	<div class="container header-full-width primary-header pt-s">
+	<div class="container primary-header pt-s">
 		<div class="header-item">
 			<a href="/" class="fw-bold fs-heading-m">Game Designers of Finland</a>
 		</div>
@@ -16,16 +16,12 @@
 	header {
 		border-bottom: 1px solid var(--grey-200);
 	}
-	
+
 	.primary-header {
 		display: flex;
 		height: var(--header-height);
 		align-items: center;
 		flex-wrap: wrap;
-	}
-
-	.header-full-width {
-		width: 100%;
 	}
 
 	a {
@@ -41,6 +37,16 @@
 		justify-content: center;
 		flex: 1;
 		flex-basis: 410px;
+	}
+
+	@media (min-width: 50em) {
+		.header-item:first-child {
+			justify-content: flex-start;
+		}
+		
+		.header-item:last-child {
+			justify-content: flex-end;
+		}
 	}
 
 </style>
