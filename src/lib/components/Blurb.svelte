10c61f9f9103a477a3888e693c2a5cef4135d5cb
Miika Savela <miika.a.savela@gmail.com>
2023-01-13 18:03:59 +0200

    Add blurb and add desires to landing page

---
+++ src/lib/components/Blurb.svelte
@@ -0,0 +1,35 @@
+<script>
+	export let isDark = true;
+
+    let blurbColor = setColor();
+    function setColor() {
+        if (isDark) {
+            return "dark";
+        }
+
+        else {
+           return "light";
+        }
+    }
+</script>
+
+<div class="blurb fs-heading-xl fw-bold {blurbColor}">
+	<slot />
+</div>
+
+<style>
+	.blurb {
+		text-align: center;
+		padding: 100px var(--p-m);
+	}
+
+	.dark {
+        background: var(--blue-700);
+        color: var(--grey-100);
+	}
+
+    .light {
+        background-color: var(--blue-500);
+        color: var(--grey-100);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Blurb from '$lib/components/Blurb.svelte';
 	import Button from '$lib/components/Button.svelte';
 	import Link from '$lib/components/Link.svelte';
 	import NextEvent from '$lib/components/NextEvent.svelte';
@@ -16,14 +17,14 @@
 </script>
 
 <section id="landing-page">
-	<div class="container pt-l pb-xxl">
+	<div class="container mt-l mb-xxl">
 		<p class="fw-bold fs-heading-l">Game design community for you</p>
 		<p class="pv-m">
-			Join the community of professional and aspiring professional game designers in Finland. Find new
-			friends and have thought provoking discussiong with fellow game design enthusiasts.
+			Join the community of professional and aspiring professional game designers in Finland. Find
+			new friends and have thought provoking discussiong with fellow game design enthusiasts.
 		</p>
 		<div class="pt-m">
-			<Button href={meetupGroupLink}>Join the group in Meetup</Button>
+			<Button href={meetupGroupLink}>Join Meetup group</Button>
 		</div>
 	</div>
 	<div
@@ -31,19 +32,53 @@
 		style="background-image: url({gdofBattleSystems})"
 	/>
 </section>
-<section class="r-20 pv-l">
-	<div class="container">
-		<h2 class="text-heading-m pb-m">Events</h2>
-		<p class="pb-s">
-			We organise free events with the best game designers in Finland share their expertise and passion
-			about variety of topics.
+<section class="">
+	<div>
+		<Blurb><h2>Game designers</h2></Blurb>
+	</div>
+	<div class="container mv-l">
+		<div>
+			<h3 class="mb-m text-heading-m">Events</h3>
+			<h4 class="fw-bold mb-s">Learn</h4>
+			<p class="mb-s">Join our free events to learn about in depth topics of game design.</p>
+			<p>
+				Expert game designers share their knowledge and experience with you, so that you can get
+				inspired in your endeavours in the game design space.
+			</p>
+			<h4 class="fw-bold mt-m mb-s">Meet</h4>
+			<p class="mb-m">
+				Connect with existing friends and meet new fellow game designers. Get inspired by new
+				viewpoints and entertain your curiosity with interesting conversations.
+			</p>
+			<Link href="#next-event" inNewTab={false}>View next event</Link>
+			<h4 class="fw-bold mt-m mb-s">Speak</h4>
+			<p>Share your knowledge and experience with other game designers by speaking in our event.</p>
+		</div>
+		<div class="mt-xxl mb-xxl">
+			<h2 class="text-heading-m mb-m">Articles</h2>
+			<p class="mb-m">
+				Learn about game design in our bite sized summaries covering game design talks and
+				interviews.
+			</p>
+			<Link href="/articles" inNewTab={false}>View articles</Link>
+		</div>
+	</div>
+	<div>
+		<Blurb isDark={false}><h2>Companies</h2></Blurb>
+	</div>
+	<div class="container mt-l mb-xxl">
+		<h3 class="mb-m text-heading-m">Host</h3>
+		<p class="mb-s">
+			Connect and reach enthusiastic game designers by hosting an event together with us.
 		</p>
-		<h2 class="text-heading-m pt-l pb-m">Articles & summaries</h2>
-		<p class="pb-m">Learn about game design in our bite sized summaries covering game design talks and interviews.</p>
-		<Link href="/articles" inNewTab={false}>View articles</Link>
+		<p>
+			Our experienced crew organises event together with you. Participate to organising as much or
+			little as you want, but we'll ensure that the event goes smooth from opening the doors to
+			closing the lights.
+		</p>
 	</div>
 </section>
-<section class="r-20 bg-secondary-100 pv-l">
+<section id="next-event" class="r-20 bg-secondary-100 pv-l">
 	<div class="container">
 		<h2 class="text-heading-m">Next event</h2>
 		{#if isNextEventAvailable}
