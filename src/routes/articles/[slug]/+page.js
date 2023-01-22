e2e0a066a8270bc3e95ef49533c1c36c8660d9c9
root <root@DESKTOP-SE242U8>
2023-01-22 15:22:47 +0200

    Add prototype of fetching likes from BE

--- src/routes/articles/[slug]/+page.js
+++ src/routes/articles/[slug]/+page.js
@@ -1,12 +1,13 @@
 import { GraphQLClient, gql } from 'graphql-request';
 
-export async function load({ params }) {
+export async function load({ fetch, params }) {
 	const hygraph = new GraphQLClient(
 		'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clc0aiadr0qx601teap7oha4h/master'
 	);
 	const query = gql`
 		query GetPostBySlug($slug: String!) {
 			post(where: { slug: $slug }) {
+				id
 				title
 				author {
 					name
@@ -23,6 +24,9 @@ export async function load({ params }) {
 	const variables = {
 		slug: params.slug
 	};
-	const post = await hygraph.request(query, variables);
-	return post;
+	const articleResponse = await hygraph.request(query, variables);
+	const article = articleResponse.post;
+	const res = await fetch(`http://localhost:5000/likes/${article.id}`);
+	const item = res.json();
+	return { article, item };
 }
